1c96cf0cf9b8024c6620c7336018deddb99861ff
HernanDarius <hdmecheverry@gmail.com>
2020-09-03 16:29:42 -0400

    Create Classes

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,24 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Room("Foyer", """Dim light filters in from the south.\n Dusty
 passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
+into the darkness.\n Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north.\n The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+chamber!/n Sadly, it has already been completely emptied by
+earlier adventurers./n The only exit is to the south."""),
 }
 
 
@@ -49,3 +49,14 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+selection = q
+while selection != q:
+    name = input("Choose Character Name: ")
+    player = Player(name, room['outside'])
+
+    print(f"{player.name} is in {player.current_room}")
+
+    selection = input("Select Direction To Travel: ")
+
+    print(f"the user selcted {selection}")
